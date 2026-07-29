f5960f544acd446b5e07cfbb989e31c0d65f686b
simplify4me <simplify4me.github@yahoo.com>
2026-07-29 00:42:34 -0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/notesdiary/sw.js', { scope: '/notesdiary/' })})}
